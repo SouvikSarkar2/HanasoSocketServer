c032aef8263d1a0375c9f744bc340c63066643c8
Souvik Sarkar <12345souvik12345@gmail.com>
2024-04-06 23:51:21 +0530

    Online Offline Logic Added

--- index.js
+++ index.js
@@ -13,16 +13,36 @@ const io = new Server(server, {
   },
 });
 
+let onlineUsers = [];
+let onlineWithLastSeen = [];
+
 io.on("connection", (socket) => {
   console.log("User Connected ", socket.id);
+
+  socket.on("offline", (data) => {
+    const updatedOnlineWithLastSeen = onlineWithLastSeen.map((user) => {
+      if (user.userId === data.userId) {
+        return { userId: data.userId, time: data.time };
+      }
+      return user;
+    });
+    console.log(updatedOnlineWithLastSeen);
+    onlineWithLastSeen = updatedOnlineWithLastSeen;
+  });
+
   socket.on("online", ({ userId, userName }) => {
     socket.join("online");
+    if (!onlineUsers.includes(userId)) {
+      onlineUsers.push(userId);
+    }
     console.log(`${userName} is Online `);
     const data = {
       userId,
       userName,
     };
-    socket.to("online").emit("onlineCheck", data);
+    io.to("online").emit("onlineCheck", onlineUsers);
+    io.to("online").emit("offlineCheck", onlineWithLastSeen);
+    console.log(onlineWithLastSeen);
   });
   socket.on("friendChanged", (userId) => {
     socket.to("online").emit("friendChanges", userId);
@@ -30,6 +50,7 @@ io.on("connection", (socket) => {
   socket.on("joinChatRoom", (roomId) => {
     socket.join(roomId);
     console.log(`user with ID ${socket.id} joined Room ${roomId}`);
+    socket.to(roomId).emit("statusCheck", { status: "online", roomId });
     socket.on("disconnect", () => {
       console.log(`user ${socket.id} Disconnected from room ${roomId} `);
     });
@@ -37,6 +58,11 @@ io.on("connection", (socket) => {
   socket.on("sendMessage", (data) => {
     socket.to(data.conversationId).emit("receiveMessage", data);
     console.log("data :", data);
+  });
+
+  socket.on("status", (data) => {
+    console.log(data.status);
+    socket.to(data.roomId).emit("statusCheck", data);
   });
 
   socket.on("disconnect", () => {
