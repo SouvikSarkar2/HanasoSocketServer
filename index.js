8411b667058bcec78a41f237d4de68593ce9b339
Souvik Sarkar <12345souvik12345@gmail.com>
2024-04-11 14:16:53 +0530

    Redis Added

--- index.js
+++ index.js
@@ -16,14 +16,7 @@ const io = new Server(server, {
   },
 });
 
-const pub = new Redis({
-  host: process.env.REDIS_HOST,
-  port: process.env.REDIS_PORT,
-  username: process.env.REDIS_USERNAME,
-  password: process.env.REDIS_PASSWORD,
-});
-
-const sub = new Redis({
+const redis = new Redis({
   host: process.env.REDIS_HOST,
   port: process.env.REDIS_PORT,
   username: process.env.REDIS_USERNAME,
@@ -33,68 +26,81 @@ const sub = new Redis({
 let onlineUsers = [];
 let onlineWithLastSeen = [];
 
-io.on("connection", (socket) => {
-  console.log("User Connected ", socket.id);
+redis.get("offline", (err, result) => {
+  if (err) {
+    console.error(err);
+  } else {
+    console.log(result);
+    onlineWithLastSeen = JSON.parse(result || "[]");
 
-  socket.on("online", ({ userId, userName }) => {
-    socket.join("online");
-    if (!onlineUsers.includes(userId)) {
-      onlineUsers.push(userId);
-    }
-    console.log(`${userName} is Online `);
+    // Setup socket.io event handling after fetching data
+    io.on("connection", (socket) => {
+      console.log("User Connected ", socket.id);
 
-    io.to("online").emit("onlineCheck", onlineUsers);
-    io.to("online").emit("offlineCheck", onlineWithLastSeen);
-
-    socket.on("disconnect", () => {
-      let present = false;
-      const updatedOnlineWithLastSeen = onlineWithLastSeen.map((user) => {
-        if (user.userId === userId) {
-          present = true;
-          return { userId: userId, time: Date.now() };
+      socket.on("online", ({ userId, userName }) => {
+        socket.join("online");
+        if (!onlineUsers.includes(userId)) {
+          onlineUsers.push(userId);
         }
-        return user;
+        console.log(`${userName} is Online `);
+
+        io.to("online").emit("onlineCheck", onlineUsers);
+        io.to("online").emit("offlineCheck", onlineWithLastSeen);
+
+        socket.on("disconnect", () => {
+          let present = false;
+          const updatedOnlineWithLastSeen = onlineWithLastSeen.map((user) => {
+            if (user.userId === userId) {
+              present = true;
+              return { userId: userId, time: Date.now() };
+            }
+            return user;
+          });
+          if (!present) {
+            updatedOnlineWithLastSeen.push({
+              userId: userId,
+              time: Date.now(),
+            });
+          }
+          onlineWithLastSeen = updatedOnlineWithLastSeen;
+          const updatedOnlineUsers = onlineUsers.filter((user) => {
+            return user !== userId;
+          });
+          onlineUsers = updatedOnlineUsers;
+
+          io.to("online").emit("onlineCheck", onlineUsers);
+          io.to("online").emit("offlineCheck", onlineWithLastSeen);
+          console.log(`${userName} is Offline`);
+          redis.set("offline", JSON.stringify(updatedOnlineWithLastSeen));
+        });
       });
-      if (!present) {
-        updatedOnlineWithLastSeen.push({ userId: userId, time: Date.now() });
-      }
-      onlineWithLastSeen = updatedOnlineWithLastSeen;
-      const updatedOnlineUsers = onlineUsers.filter((user) => {
-        return user !== userId;
+      socket.on("friendChanged", (userId) => {
+        socket.to("online").emit("friendChanges", userId);
       });
-      onlineUsers = updatedOnlineUsers;
+      socket.on("joinChatRoom", async (roomId) => {
+        socket.join(roomId);
 
-      io.to("online").emit("onlineCheck", onlineUsers);
-      io.to("online").emit("offlineCheck", onlineWithLastSeen);
-      console.log(`${userName} is Offline`);
+        console.log(`user with ID ${socket.id} joined Room ${roomId}`);
+        socket.to(roomId).emit("statusCheck", { status: "online", roomId });
+        socket.on("disconnect", () => {
+          console.log(`user ${socket.id} Disconnected from room ${roomId} `);
+        });
+      });
+      socket.on("sendMessage", (data) => {
+        socket.to(data.conversationId).emit("receiveMessage", data);
+        io.to(data.conversationId).emit("receiveLastMessage", data);
+        console.log("data :", data);
+      });
+
+      socket.on("status", (data) => {
+        socket.to(data.roomId).emit("statusCheck", data);
+      });
+
+      socket.on("disconnect", () => {
+        console.log("user Disconnected ", socket.id);
+      });
     });
-  });
-  socket.on("friendChanged", (userId) => {
-    socket.to("online").emit("friendChanges", userId);
-  });
-  socket.on("joinChatRoom", async (roomId) => {
-    socket.join(roomId);
-    await pub.publish(`${roomId}`, JSON.stringify({ roomId }));
-
-    console.log(`user with ID ${socket.id} joined Room ${roomId}`);
-    socket.to(roomId).emit("statusCheck", { status: "online", roomId });
-    socket.on("disconnect", () => {
-      console.log(`user ${socket.id} Disconnected from room ${roomId} `);
-    });
-  });
-  socket.on("sendMessage", (data) => {
-    socket.to(data.conversationId).emit("receiveMessage", data);
-    io.to(data.conversationId).emit("receiveLastMessage", data);
-    console.log("data :", data);
-  });
-
-  socket.on("status", (data) => {
-    socket.to(data.roomId).emit("statusCheck", data);
-  });
-
-  socket.on("disconnect", (data) => {
-    console.log("user Disconnected ", socket.id);
-  });
+  }
 });
 
 server.listen(3001, () => {
